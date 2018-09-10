597573289790b43a755f9c020d50333583c1cff9
Juozas Gaigalas <juozasgaigalas@gmail.com>
2018-09-10 15:06:30 -0400

    clean up records import

--- records-import.sh
+++ records-import.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-chown -R 1000:1000 records
+set -x
+sudo chown -R 1000:1000 records
 
 echo clear records
 docker exec -ti pycsw  pycsw-admin.py -c delete_records -f /etc/pycsw/pycsw.cfg -y
 
-echo import records
-docker exec -ti pycsw  pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p /records/harvested -r -y
+echo import granule records
+docker exec -ti pycsw  pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p /records/granules -r -y
+
+echo import collections records
+docker exec -ti pycsw  pycsw-admin.py -c load_records -f /etc/pycsw/pycsw.cfg -p /records/collections -r -y
 
