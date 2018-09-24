c5a973b106a42b1c621761bd3f04d21a16e57300
Juozas Gaigalas <juozasgaigalas@gmail.com>
2018-09-24 10:31:07 -0400

    example sql code

--- harvester/index_collection_granules.py
+++ harvester/index_collection_granules.py
@@ -35,6 +35,35 @@ results = indexer.indexes
 print(results)
 
 
+# CREATE TABLE `catalogs` (
+#   `id` int(10) NOT NULL AUTO_INCREMENT,
+#   `parent_catalog_id` int(11) NOT NULL DEFAULT '1',
+#   `name` varchar(512) NOT NULL DEFAULT '',
+#   `url` varchar(512) NOT NULL DEFAULT '',
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+# CREATE TABLE `datasets` (
+#   `id` int(11) NOT NULL AUTO_INCREMENT,
+#   `catalog_id` int(10) NOT NULL,
+#   `name` varchar(512) NOT NULL DEFAULT '''',
+#   `url_path` varchar(512) NOT NULL,
+#   `iso_url` varchar(512) NOT NULL,
+#   PRIMARY KEY (`id`)
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+# import records
+# db = records.Database('mysql://root@localhost/thredds_radar_analysis')
+
+# def reset_db():
+#     db.query('delete from catalogs')
+#     db.query('ALTER TABLE catalogs AUTO_INCREMENT = 1')
+#     db.query('delete from datasets')
+#     db.query('ALTER TABLE datasets AUTO_INCREMENT = 1')
+#     top_cat.save_catalog_to_db(0)
+
 
 # write results to SQL database here
 # write_to_sql(indexer.results, collection_catalog_url, collection_name)
