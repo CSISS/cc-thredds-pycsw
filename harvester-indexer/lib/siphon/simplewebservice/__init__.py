b5bdd45cea6c7ea98d99df09cbcc879271428860
Juozas Gaigalas <juozasgaigalas@gmail.com>
2018-12-05 14:55:21 -0500

    remove code that went to another repo. update docker compose

--- harvester-indexer/lib/siphon/simplewebservice/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2013-2015 Siphon Contributors.
-# Distributed under the terms of the BSD 3-Clause License.
-# SPDX-License-Identifier: BSD-3-Clause
-"""Contains functionality for retrieving simple web service data."""
