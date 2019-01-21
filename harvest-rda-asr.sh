258af95f9a41ce8ea883c35d733cb445a1ccbc6a
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-01-20 23:54:50 -0500

    harvest RDA 631.1

--- harvest-rda-asr.sh
+++ harvest-rda-asr.sh
@@ -2,18 +2,16 @@
 
 set -x
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30fnl.anl.2D/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.anl.2D/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30fnl.anl.3D/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.anl.3D/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30fnl.anl.monthly/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.anl.monthly/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30fnl.fcst3.2D/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.fcst3.2D/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30fnl.fcst3.3D/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.fcst3.3D/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30fnl.fcst3.monthly/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.fcst3.monthly/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asr30km.invariant/catalog.xml"
-
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.0/asrfinal30km.invariant/catalog.xml"
+curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/invariant/catalog.xml"
