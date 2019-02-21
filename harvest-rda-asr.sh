e7a1a02d2dd4fdfbbf56e0620a141435b888ca5f
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-02-20 20:39:23 -0500

    collection level asr

--- harvest-rda-asr.sh
+++ harvest-rda-asr.sh
@@ -2,16 +2,5 @@
 
 set -x
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.anl.2D/catalog.xml"
+curl http://localhost:8002/index -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/catalog.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.anl.3D/catalog.xml"
-
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.anl.monthly/catalog.xml"
-
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.fcst3.2D/catalog.xml"
-
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.fcst3.3D/catalog.xml"
-
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/asr15.fcst3.monthly/catalog.xml"
-
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=https://rda.ucar.edu/thredds/catalog/files/g/ds631.1/invariant/catalog.xml"
