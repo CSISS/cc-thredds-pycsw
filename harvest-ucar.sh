3dede69738824e270a39334376362d611ce43f25
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-02-21 14:38:51 -0500

    use new API for index for ucar

--- harvest-ucar.sh
+++ harvest-ucar.sh
@@ -2,14 +2,13 @@
 
 set -x
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/forecastModels.xml"
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/forecastModels.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/forecastProdsAndAna.xml"
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/forecastProdsAndAna.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/obsData.xml"
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/obsData.xml"
 
-curl http://localhost:8002/harvest/collections -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/radars.xml"
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/radars.xml"
 
-curl http://localhost:8002/harvest/granules -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/satellite.xml"
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/satellite.xml"
 
-curl http://localhost:8002/purge_expired -X POST
