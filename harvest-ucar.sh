063db4389baa21769e2de0426ff29f9525e5081b
Juozas Gaigalas <juozasgaigalas@gmail.com>
2019-02-28 14:47:41 -0500

    harvest radar again

--- harvest-ucar.sh
+++ harvest-ucar.sh
@@ -8,19 +8,16 @@ curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu
 
 curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/obsData.xml"
 
-# curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/radars.xml"
-
-
-#curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/nexrad/level3/catalog.xml"
-
-#curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/terminal/level3/catalog.xml"
-
-#curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/nexrad/level2/catalog.xml"
-
-#curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/nexrad/composite/gini/catalog.xml"
-
-#curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/grib/nexrad/composite/unidata/catalog.xml"
-
-
 curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/idd/satellite.xml"
 
+
+# RADARS
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/nexrad/level3/catalog.xml"
+
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/terminal/level3/catalog.xml"
+
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/nexrad/level2/catalog.xml"
+
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/nexrad/composite/gini/catalog.xml"
+
+curl http://localhost:8002/index -X POST -d "catalog_url=http://thredds.ucar.edu/thredds/catalog/grib/nexrad/composite/unidata/catalog.xml"
